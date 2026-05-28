61f15ca5f3f74148ced3c4881228235d4363c000
ManuVargheseGit <varghesemanu5@gmail.com>
2026-05-28 09:25:53 +0200

    Fix GitHub Pages root paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? "/tenderlyte" : "";
-
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   outputFileTracingRoot: new URL(".", import.meta.url).pathname,
   output: "export",
   trailingSlash: true,
-  basePath,
-  assetPrefix: basePath,
-  env: {
-    NEXT_PUBLIC_SITE_BASE_PATH: basePath
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
